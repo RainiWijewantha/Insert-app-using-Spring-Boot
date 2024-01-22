156ffd371066560de1bac0c13726e92fdf368f69
RainiWijewantha <112390022+RainiWijewantha@users.noreply.github.com>
2024-01-22 11:05:11 +0530

    Add files via upload

---
+++ src/main/java/com/studentexample/StudentExampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.studentexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentExampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentexample/StudentExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentExampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
